dcda165e52422abed9ec5eb7c63d5927d7c9381c
s.shynkaruk <Sergiy.shynkaruk@gmail.com>
2016-06-04 21:42:00 +0300

    create dir hierarchy

---
+++ source/Application/ArmorEngine.cpp
@@ -0,0 +1,15 @@
+// ArmorEngine.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+__declspec(dllexport) int a()
+{
+	return 42;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
